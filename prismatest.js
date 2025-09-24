8ea864e658f49cf94e3ff4fb674f25f22e3ade66
Dxote <157197390+Dxote@users.noreply.github.com>
2025-09-25 04:27:00 +0700

    Delete prismatest.js

--- prismatest.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-async function main() {
-  const allUsers = await prisma.user.findMany(); // ganti dengan tabel yang ada
-  console.log(allUsers);
-}
-
-main().catch(e => console.error(e));
